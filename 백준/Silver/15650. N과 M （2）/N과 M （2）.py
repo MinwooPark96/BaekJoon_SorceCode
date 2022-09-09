685fe030f838b12b7f26c183a81a32a100928b7d
Park Min woo <99009977+MinwooPark96@users.noreply.github.com>
2022-09-10 00:46:50 +0900

    [Silver III] Title: N과 M (2), Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,19 @@
+answer=[]
+curmax=0
+def sortchoose(depth,n,m,curmax):
+    if depth==0:
+        for i in answer:
+            print(i,end=" ")
+        print()
+        
+        return
+    for i in range(1,n+1): #1~n
+        if i>curmax:
+            answer.append(i)
+            curmax=i
+            sortchoose(depth-1,n,m,curmax)
+            answer.pop()
+            curmax=0
+            
+n,m=map(int,input().split(" "))
+sortchoose(m,n,m,curmax)
