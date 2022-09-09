c078b3d38779d50d7e587dd2dd56e17d6a49cd33
Park Min woo <99009977+MinwooPark96@users.noreply.github.com>
2022-09-10 00:43:30 +0900

    [Bronze I] Title: 이항 계수 1, Time: 96 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -0,0 +1,14 @@
+n,k=map(int,input().split(" "))
+
+choose=[[0]*(k+2) for i in range(n+1)]
+for i in range(n+1):
+    for j in range(k+1):
+        if i==j:
+            choose[i][j]=1
+        if j==0:
+            choose[i][j]=1
+for i in range(1,n+1):
+    for j in range(1,k+1):
+        choose[i][j]=choose[i-1][j-1]+choose[i-1][j]
+
+print(choose[n][k])
