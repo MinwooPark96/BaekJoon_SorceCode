8fd0502665c80b701bab636fd8a459647b12f1b6
Park Min woo <99009977+MinwooPark96@users.noreply.github.com>
2022-09-10 00:14:31 +0900

    [Silver V] Title: 뒤집기, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -0,0 +1,12 @@
+string=input()
+list0=string.split("0")
+list1=string.split("1")
+count0=0
+count1=0
+for i in list0:
+    if i :
+        count0+=1
+for i in list1:
+    if i :
+        count1+=1
+print(min(count0,count1))
