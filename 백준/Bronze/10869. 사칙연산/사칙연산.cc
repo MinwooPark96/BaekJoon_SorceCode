98aa165a450876dd33b48684731547e1e70d433d
Park Min woo <99009977+MinwooPark96@users.noreply.github.com>
2022-10-09 22:51:03 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,55 @@
+//
+//  main.cpp
+//  BaekJoon
+//
+//  Created by Minwoo on 2022/09/23.
+//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+//#include <set>
+#include <map>
+//#include <unordered_map>
+#include <deque>
+#include <list>
+
+//using std::cout;
+//using std::cin;
+
+// 백준 14500 번
+
+
+/*------------------function------------*/
+//정답 변수
+
+
+/*-------------function-----------------*/
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    
+    /*-----------code-----------------*/
+    int a,b; std::cin>>a>>b;
+    
+    std::cout<<a+b<<"\n";
+    std::cout<<a-b<<"\n";
+    std::cout<<a*b<<"\n";
+    std::cout<<a/b<<"\n";
+    std::cout<<a%b<<"\n";
+    
+    
+    
+    
+    /*-----------code-----------------*/
+
+    
+    return 0;
+}
+
+/*------------------function------------*/
+
+
+
+
+
