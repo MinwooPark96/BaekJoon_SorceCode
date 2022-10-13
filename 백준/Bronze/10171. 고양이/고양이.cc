744baf1c646ce9d3be7816281e934a8d3b0875af
Park Min woo <99009977+MinwooPark96@users.noreply.github.com>
2022-10-13 13:41:58 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,50 @@
+//
+//  main.cpp
+//  BaekJoon
+//
+//  Created by Minwoo on 2022/09/23.
+//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+//#include <functional>
+//#include <map>
+//#include <unordered_map>
+#include <deque>
+//#include <list>
+//#include <queue>
+//using std::cout;
+//using std::cin;
+#include <math.h>
+// 백준  번
+
+
+/*------------------function------------*/
+//정답 변수
+
+
+
+
+
+/*-------------function-----------------*/
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    
+    /*-----------code-----------------*/
+    std::cout<<"\\    /\\"<<"\n";
+    std::cout<<" )  ( ')"<<"\n";
+    std::cout<<"(  /  )"<<"\n";
+    std::cout<<" \\(__)|"<<"\n";
+    
+    /*-----------code-----------------*/
+
+    
+    
+    return 0;
+}
+
+/*------------------function------------*/
+
