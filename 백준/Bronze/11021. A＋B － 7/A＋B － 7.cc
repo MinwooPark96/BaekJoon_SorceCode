055746cd81a8053ae8965e44e79516ad6ed97dec
Park Min woo <99009977+MinwooPark96@users.noreply.github.com>
2022-10-16 09:46:36 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,83 @@
+//
+//  main.cpp
+//  BaekJoon
+//
+//  Created by Minwoo on 2022/09/23.
+//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+//#include <functional>
+//#include <map>
+//#include <unordered_map>
+#include <deque>
+//#include <list>
+//#include <queue>
+//using std::cout;
+//using std::cin;
+#include <math.h>
+
+/*------------------function------------*/
+//정답 변수
+
+
+
+
+
+/*-------------function-----------------*/
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    
+    /*-----------code-----------------*/
+
+    int n;std::cin>>n;
+    for (int i=0;i<n;i++){
+        int a,b;
+        std::cin>>a>>b;
+        std::cout<<"Case #"<<i+1<<": "<<a+b<<"\n";
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*-----------code-----------------*/
+
+    
+    
+    return 0;
+}
+
+/*------------------function------------*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*-------------function-----------------*/
+
