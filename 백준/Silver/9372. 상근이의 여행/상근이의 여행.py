dbd027214e6db8e3af60c0a2f0b90668223deb1e
Park Min woo <99009977+MinwooPark96@users.noreply.github.com>
2022-12-02 19:41:13 +0900

    [Silver IV] Title: 상근이의 여행, Time: 160 ms, Memory: 114976 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -0,0 +1,12 @@
+import sys
+input=sys.stdin.readline
+n=int(input())
+
+for _ in range(n):
+    v,e=map(int,input().split())
+    for _ in range(e):
+        v1,v2=map(int,input().split())
+    print(v-1)    
+        
+    
+    
