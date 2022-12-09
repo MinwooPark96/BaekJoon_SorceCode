3c9a33547f22337abd7bb3d8f44cf935dcb353e9
Park Min woo <99009977+MinwooPark96@users.noreply.github.com>
2022-12-09 15:27:36 +0300

    [Gold V] Title: 최소비용 구하기, Time: 644 ms, Memory: 139124 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -0,0 +1,31 @@
+import sys
+import heapq
+#input=sys.stdin.readline
+pQ=[]
+inf=int(1e9)
+V=int(input())
+E=int(input())
+Edges=[[] for i in range(V+1)]
+visit=[0 for _ in range(V+1)]
+distance=[inf for i in range(V+1)]
+
+for _ in range(E):
+    v1,v2,w=map(int,input().split())
+    Edges[v1].append([v2,w])
+s,f=map(int,input().split())
+distance[s]=0
+
+heapq.heappush(pQ,[distance[s],s])
+while pQ:
+    weight_u,u=heapq.heappop(pQ)
+    if visit[u]==1:
+        continue
+    visit[u]=1
+    for v,w in Edges[u]:
+        flag=0
+        if distance[u]!=inf and distance[v]>distance[u]+w:
+            flag=1
+            distance[v]=distance[u]+w
+        if flag==1:
+            heapq.heappush(pQ,[distance[v],v])
+print(distance[f])
