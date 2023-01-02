5a1ffecb080838975b1a5e7822cf8e12edbc50ba
Park Min woo <99009977+MinwooPark96@users.noreply.github.com>
2023-01-02 18:06:11 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cc
@@ -0,0 +1,72 @@
+//
+//  main.cpp
+//  BaekJoon
+//
+//  Created by Minwoo on 2022/09/23.
+//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+//#include <functional>
+#include <map>
+//#include <unordered_map>
+#include <deque>
+#include <list>
+//#include <queue>
+//using std::cout;
+//using std::cin;
+#include <math.h>
+
+/*----------------macro--------------*/
+
+#define rep(i,a,b) for (int i = (a); i < (b); ++i)
+#define debug(x) std::cout << #x << " is " << x << '\n'
+#define print(x) std::cout << x <<'\n'
+#define input(x) std::cin >> x
+
+
+/*----------------macro--------------*/
+/*------------------function------------*/
+//정답 변수
+
+
+
+
+
+
+
+/*-------------function-----------------*/
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    
+    
+    
+    
+    /*-----------code-----------------*/
+    std::vector<int> visit(31,0);
+    rep(i,0,28){
+        int x;input(x);
+        visit[x]=1;
+    }
+    
+    rep(i,1,31){
+        if (visit[i]==0){
+            print(i);
+        }
+    }
+    /*-----------code-----------------*/
+    
+    
+    
+    return 0;
+    
+}
+/*------------------function------------*/
+
+
+
+/*-------------function-----------------*/
+
