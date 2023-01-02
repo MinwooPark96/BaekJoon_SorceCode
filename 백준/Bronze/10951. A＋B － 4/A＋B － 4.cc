b7e2aba115fd6950e57dc9e8bde4269923c72b0f
Park Min woo <99009977+MinwooPark96@users.noreply.github.com>
2023-01-02 13:37:02 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,74 @@
+//
+//  main.cpp
+//  BaekJoon
+//
+//  Created by Minwoo on 2022/09/23.
+//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+//#include <functional>
+#include <map>
+//#include <unordered_map>
+#include <deque>
+#include <list>
+//#include <queue>
+//using std::cout;
+//using std::cin;
+#include <math.h>
+
+/*----------------macro--------------*/
+
+#define rep(i,a,b) for (int i = (a); i < (b); ++i)
+#define debug(x) std::cout << #x << " is " << x << '\n'
+#define print(x) std::cout << x <<'\n'
+#define input(x) std::cin >> x
+
+
+/*----------------macro--------------*/
+/*------------------function------------*/
+//정답 변수
+
+
+
+
+
+
+
+
+
+/*-------------function-----------------*/
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    
+    
+    
+    
+    /*-----------code-----------------*/
+    while(1){
+        int a,b;
+        int sum=0;
+        input(a);input(b);
+        if (std::cin.eof() == true){
+            break;
+        }
+        sum=a+b;
+        print(sum);
+    }
+    
+    /*-----------code-----------------*/
+    
+    
+    
+    return 0;
+    
+}
+/*------------------function------------*/
+
+
+
+/*-------------function-----------------*/
+
