21a1db4f08bdbe65c9749eed2256aafee1101dba
Park Min woo <99009977+MinwooPark96@users.noreply.github.com>
2023-01-02 12:56:21 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,70 @@
+//
+//  main.cpp
+//  BaekJoon
+//
+//  Created by Minwoo on 2022/09/23.
+//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+//#include <functional>
+#include <map>
+//#include <unordered_map>
+#include <deque>
+#include <list>
+//#include <queue>
+//using std::cout;
+//using std::cin;
+#include <math.h>
+
+/*----------------macro--------------*/
+
+#define rep(i,a,b) for (int i = (a); i < (b); ++i)
+#define debug(x) std::cout << #x << " is " << x << '\n'
+#define print(x) std::cout << x << '\n'
+#define input(x) std::cin >> x
+
+/*----------------macro--------------*/
+/*------------------function------------*/
+//정답 변수
+
+
+
+
+
+
+
+
+
+/*-------------function-----------------*/
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    
+    
+    
+    
+    /*-----------code-----------------*/
+    int n;
+    int sum=0;
+    input(n);
+    rep(i, 1, n+1){
+        sum+=i;
+    }
+    print(sum);
+    /*-----------code-----------------*/
+
+    
+    
+    return 0;
+    
+}
+
+/*------------------function------------*/
+
+
+
+/*-------------function-----------------*/
+
