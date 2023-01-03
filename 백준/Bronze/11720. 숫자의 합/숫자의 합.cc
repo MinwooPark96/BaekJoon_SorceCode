604a2a05b36f12d79105ccc323beb216f8a83b0a
Park Min woo <99009977+MinwooPark96@users.noreply.github.com>
2023-01-03 10:44:28 +0900

    [Bronze IV] Title: 숫자의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
@@ -0,0 +1,75 @@
+//
+//  main.cpp
+//  BaekJoon
+//
+//  Created by Minwoo on 2022/09/23.
+//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+//#include <functional>
+#include <map>
+//#include <unordered_map>
+#include <deque>
+#include <list>
+//#include <queue>
+//using std::cout;
+//using std::cin;
+#include <math.h>
+#include <numeric>
+#include <sstream>
+/*----------------macro--------------*/
+
+#define rep(i,a,b) for (int i = (a); i < (b); ++i)
+#define debug(x) std::cout << #x << " is " << x << '\n'
+#define print(x) std::cout << x
+#define input(x) std::cin >> x
+
+
+/*----------------macro--------------*/
+/*------------------function------------*/
+//정답 변수
+
+
+
+
+
+
+
+
+
+/*-------------function-----------------*/
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    
+    
+    
+    
+    /*-----------code-----------------*/
+    int n; input(n);
+    int answer=0;
+    char c;
+    rep(i,0,n){
+        input(c);
+        answer+=c-48;
+    }
+    print(answer);
+        
+    
+    
+    /*-----------code-----------------*/
+    
+    
+    
+    return 0;
+    
+}
+/*------------------function------------*/
+
+
+
+/*-------------function-----------------*/
+
