b3e367d4ca5f8c2a394ccc6a5b96bc61aa19eee8
Park Min woo <99009977+MinwooPark96@users.noreply.github.com>
2023-01-03 11:38:31 +0900

    [Bronze II] Title: 다이얼, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.cc
@@ -0,0 +1,78 @@
+//
+//  main.cpp
+//  BaekJoon
+//
+//  Created by Minwoo on 2022/09/23.
+//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+//#include <functional>
+#include <map>
+//#include <unordered_map>
+#include <deque>
+#include <list>
+//#include <queue>
+//using std::cout;
+//using std::cin;
+#include <math.h>
+#include <numeric>
+#include <sstream>
+/*----------------macro--------------*/
+
+#define rep(i,a,b) for (int i = (a); i < (b); ++i)
+#define debug(x) std::cout << #x << " is " << x << '\n'
+#define print(x) std::cout << x <<' '
+#define input(x) std::cin >> x
+
+
+/*----------------macro--------------*/
+/*------------------function------------*/
+//정답 변수
+
+
+
+
+
+
+
+
+
+/*-------------function-----------------*/
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    
+    
+    
+    
+    /*-----------code-----------------*/
+    
+     
+    int arr[26] = { 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10, 10 };
+    int sum = 0;
+    std::string s;
+ 
+    std::cin >> s;
+ 
+    for (int i = 0; i < s.length(); ++i) {
+        int index = s[i] - 65;
+        sum += arr[index];
+    }
+ 
+    print(sum);
+    /*-----------code-----------------*/
+    
+    
+    
+    return 0;
+    
+}
+/*------------------function------------*/
+
+
+
+/*-------------function-----------------*/
+
