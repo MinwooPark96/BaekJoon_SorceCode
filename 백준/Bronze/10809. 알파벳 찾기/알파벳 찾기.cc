9993635c56c7cad8a51cf1c8a0831c666fed1713
Park Min woo <99009977+MinwooPark96@users.noreply.github.com>
2023-01-03 11:08:00 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
@@ -0,0 +1,83 @@
+//
+//  main.cpp
+//  BaekJoon
+//
+//  Created by Minwoo on 2022/09/23.
+//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+//#include <functional>
+#include <map>
+//#include <unordered_map>
+#include <deque>
+#include <list>
+//#include <queue>
+//using std::cout;
+//using std::cin;
+#include <math.h>
+#include <numeric>
+#include <sstream>
+/*----------------macro--------------*/
+
+#define rep(i,a,b) for (int i = (a); i < (b); ++i)
+#define debug(x) std::cout << #x << " is " << x << '\n'
+#define print(x) std::cout << x <<' '
+#define input(x) std::cin >> x
+
+
+/*----------------macro--------------*/
+/*------------------function------------*/
+//정답 변수
+
+
+
+
+
+
+
+
+
+/*-------------function-----------------*/
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    
+    
+    
+    
+    /*-----------code-----------------*/
+    
+    std::vector<int> a(26,-1);
+    std::string s;
+    input(s);
+    
+    rep(i,0,s.size()){
+        int x = s[i] - 'a';
+        if (a[x]==-1){
+            a[x]=i;
+        }
+    }
+    
+    rep(i,0,26){
+        print(a[i]);
+    }
+    
+    
+    
+    
+    /*-----------code-----------------*/
+    
+    
+    
+    return 0;
+    
+}
+/*------------------function------------*/
+
+
+
+/*-------------function-----------------*/
+
